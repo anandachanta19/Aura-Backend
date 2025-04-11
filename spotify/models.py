065a8967fbe92f57d6a74e2c78806ba1403b7c7d
anandachanta19 <anandachanta19@gmail.com>
2025-04-11 14:27:00 +0530

    Token Length Issues Resolve

--- spotify/models.py
+++ spotify/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class SpotifyToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     session_key = models.CharField(max_length=40)  # Store Django session key
-    access_token = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
+    access_token = models.CharField()
+    refresh_token = models.CharField()
     expires_at = models.DateTimeField()
     token_type = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
